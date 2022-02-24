aa8a025fea16ece4710725e38ca13782d0cf5c17
sporkupine <coffeechief25@gmail.com>
2022-02-24 14:27:30 -0600

    authenticator mostly works

--- authenticator.rb
+++ authenticator.rb
@@ -1,13 +1,33 @@
+users = [
+    { username: "sporkupine", password: "password1" },
+    { username: "derekjeter", password: "password2" },
+    { username: "timmyturner", password: "password3" },
+    { username: "the_dude", password: "abides" }
+]
+
+def auth_user(username, password, user_list)
+    user_list.each do |user|
+        if user[:username] == username && user[:password] == password
+            return "Credentials verified. Welcome, #{user[:username]}"
+        end
+     end
+     return "Invalid credentials. Please try again."
+end
+
 puts "Initialize authentication protocol"
 25.times { print "-" }
-puts ""
-puts "Please enter your credentials. \nUsername:"
-username = gets.chomp
-puts "Password:"
-password = gets.chomp
+print "\n"
+puts "Please enter your credentials."
 
-if username.downcase == "sporkupine" && password == "password1"
-    puts "Credentials verified. Welcome Sporkupine."
-else 
-    puts "Invalid credentials. Please try again."
+count = 1
+
+while count < 4 do
+    print "Username: "
+    username = gets.chomp
+    print "Password: "
+    password = gets.chomp
+    auth = auth_user(username, password, users)
+    puts auth
+    count += 1
 end
+puts "You have exceeded the maximum number of failed validation attempts. \nGoodbye." if count == 4
